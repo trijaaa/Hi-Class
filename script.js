3d0c86f8ef421f6f1125a7c68fc162bc34501cb7
trijaaa <trizasafwanalzahran@gmail.com>
2025-11-05 19:33:49 +0700

    link script di htmlnya

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Gua bikin JS nunggu html selesai!
+const kelompok = "Kelompok 2 Hi-Class";
+let nama = {
+    nama1: "Azzahra",
+    nama2: "Fadhil",
+    nama3: "Awan",
+    nama4: "Raihan",
+    nama5: "Triza",
+};
+
+console.log(kelompok)
+console.log(nama)
