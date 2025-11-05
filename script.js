bd49b03e3803beb07ca5fdb8c6f11f166c7d73d8
trijaaa <trizasafwanalzahran@gmail.com>
2025-11-05 20:41:40 +0700

    js lagi

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const kelompok = "Kelompok 2 Hi-Class";
+const kelompok = "Kelompok 2 Hi-Class"
 let nama = {
     nama1: "Azzahra",
     nama2: "Fadhil",
     nama3: "Awan",
     nama4: "Raihan",
-    nama5: "Triza",
-};
+    nama5: "Triza"
+}
 
 console.log(kelompok)
-console.log(nama)
+console.table(nama)
